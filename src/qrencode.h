b99bdffc1c3934ee102b05f47453b728260b7aa4
AdamAZCS <adam@azcs.at>
2022-07-03 17:05:41 +0200

    added __xtensa__ section to include pgmspace.h and define __LPM(x) macro on ESP32 boards

--- src/qrencode.h
+++ src/qrencode.h
@@ -1,12 +1,18 @@
-#ifndef __AVR__
+#ifdef __AVR__
+#include <avr/pgmspace.h>
+#define USEPRECALC
+#elif defined(__xtensa__) 
+#   include <pgmspace.h>
+#   ifndef __LPM
+#       define __LPM(x) *x
+#   endif
+#else 
 #define PROGMEM
 #define memcpy_P memcpy
 #define __LPM(x) *x
 #define pgm_read_word(x) *x
-#else
-#include <avr/pgmspace.h>
-#define USEPRECALC
 #endif
+
 
 extern unsigned char strinbuf[];
 extern unsigned char qrframe[];
